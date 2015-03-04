9c16d3c1e516f089c1ef5b3339126ba336dab203
BJ Johnson <rndmbnjmn@gmail.com>
2015-03-04 10:02:08 -0500

    1. added subheader 2. adjusted templates and css for footer nav

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template as rendTemp
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return rendTemp('home.html', subheader='Home')
