9bdc59ce5b43c052f270d5764c60f484355c6aa9
BJ Johnson <rndmbnjmn@gmail.com>
2015-03-04 14:11:39 -0500

    1. finished design of profile page with floating footer

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,16 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return rendTemp('home.html', subheader='Home')
+    return rendTemp('home.html', header='Benjamin (BJ) Johnson')
+
+@app.route('/profile')
+def profile():
+    return rendTemp('profile.html', header='Profile')
+
+@app.route('/portfolio')
+def port():
+    return rendTemp('portfolio.html', header='Portfolio')
+
+@app.route('/certs')
+def certs():
+    return rendTemp('certs.html', header='Certifications')
